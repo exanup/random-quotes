9fcf324dbfcfb2fab47c7b6e9dc1c15097031201
Anup Dhakal <20139982+exanup@users.noreply.github.com>
2018-12-09 10:29:39 +0545

    remove console logs

--- src/index.js
+++ src/index.js
@@ -7,14 +7,9 @@ const app = Elm.Main.init({
   node: document.getElementById('root')
 });
 
-console.log(app.ports);
-
 app.ports.askForUniqueId.subscribe(() => {
-  console.log('Elm is asking for a unique ID.');
-
   const id = uuid();
 
-  console.log('Sending id: ' + id);
   app.ports.uniqueId.send(id);
 });
 
