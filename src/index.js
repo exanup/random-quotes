a578b39fc4fefe31865b9259933a9a5d457a4395
Anup Dhakal <20139982+exanup@users.noreply.github.com>
2018-12-09 10:06:33 +0545

    try out some ports

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import './main.css';
+import uuid from './uniqueId';
 import { Elm } from './Main.elm';
 import registerServiceWorker from './registerServiceWorker';
 
-Elm.Main.init({
+const app = Elm.Main.init({
   node: document.getElementById('root')
 });
+
+console.log(app.ports);
+
+// app.ports.askForUniqueId.subscribe(() => {
+//   console.log('Elm is asking for a unique ID.');
+
+//   const id = uuid();
+
+//   console.log('Sending id: ' + id);
+//   app.ports.uniqueId.send(id);
+// });
 
 registerServiceWorker();
